322835bc093ec9d2a8d17fe5773891139c62fd9a
Dimid Duchovny <dimidd@gmail.com>
2015-10-28 08:19:56 +0200

    events: init
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- cal.py
+++ cal.py
@@ -4,11 +4,15 @@ import calendar
 import time
 import os
 import pathlib
+import xmltodict
+from urllib.request import urlopen
 
 CAL = calendar.HTMLCalendar(calendar.SUNDAY)
 Y, M = time.strftime('%Y %m').split()
 BASE_PATH = pathlib.Path(__file__).parent
 PORT = int(os.environ.get("PORT", 49229))
+HIST_PREF = "http://www.vizgr.org/historical-events/search.php?"
+HIST_SUFF = "&html=true&format=xml"
 MIN_YEAR = 2
 MAX_YEAR = 9999
 
@@ -40,14 +44,27 @@ def get_prev(year, month=None):
 
 
 @route('/')
-@route('/<year>/<month:re:\d{1,2}/>')
-def mcal(year=Y, month=M):
-    year = int(year)
-    month = int(month[:-1])
+@route('/<year_s>/<month_s:re:\d{1,2}/>')
+def mcal(year_s=Y, month_s=M):
+    month_s = month_s if month_s[-1] != '/' else month_s[:-1]
+    year = int(year_s)
+    month = int(month_s)
+    cal = CAL.formatmonth(year, month, True)
     n_y, n_m = get_next(year, month)
     p_y, p_m = get_prev(year, month)
     ad = [p_y, p_m, n_y, n_m]
-    return template('cal', cal=CAL.formatmonth(year, month, True), adjacent=ad)
+    pad_y = ((4 - len(year_s)) * '0') + year_s
+    pad_m = ((2 - len(month_s)) * '0') + month_s
+    url_mid = "begin_date={}{}01&end_date={}{}31".format(pad_y, pad_m, pad_y, pad_m)
+    ev_file = urlopen(HIST_PREF + url_mid + HIST_SUFF)
+    data = ev_file.read()
+    ev_file.close()
+    events = []
+    if data != b'No events found for this query.':
+        events_xml = xmltodict.parse(data)
+        events = events_xml["result"]["event"]
+        events = [events] if type(events) != list else events
+    return template('cal', cal=cal, adjacent=ad, events=events)
 
 
 @route('/<year:re:\d{4}/>')
--- views/cal.tpl
+++ views/cal.tpl
@@ -1,24 +1,39 @@
 <head>
-<link rel="stylesheet" type="text/css" href="/assets/cal.css/"/>
+  % import re
+  <link rel="stylesheet" type="text/css" href="/assets/cal.css/"/>
 </head>
+
 <body>
-<h1>Cal</h1>
-<div> {{!cal}} </div>
-<% p_y, p_m, n_y, n_m = adjacent
- if p_y:
-     if p_m:
-%>
-        <a href="/{{p_y}}/{{p_m}}/">Prev</a>
-%     else:
-        <a href="/{{p_y}}/">Prev</a>
-%     end
-% end
-<% if n_y:
-     if n_m:
-%>
-        <a href="/{{n_y}}/{{n_m}}/">Next</a>
-%    else:
-        <a href="/{{n_y}}/">Next</a>
-%    end
-% end
+  <h2>HackiCal</h2>
+  <div> {{!cal}} </div>
+  <div>
+    <% p_y, p_m, n_y, n_m = adjacent
+     if p_y:
+         if p_m:
+    %>
+            <a href="/{{p_y}}/{{p_m}}/">Prev</a>
+    %     else:
+            <a href="/{{p_y}}/">Prev</a>
+    %     end
+    % end
+    <% if n_y:
+         if n_m:
+    %>
+            <a href="/{{n_y}}/{{n_m}}/">Next</a>
+    %    else:
+            <a href="/{{n_y}}/">Next</a>
+    %    end
+    % end
+  </div>
+<div>
+  <h2>Historical Events</h2>
+  <ol>
+  % for e in events:
+    % desc = re.split(r'\.(ref|{|<a href|amp)', e["description"])[0]
+    % desc = desc.replace('ampndash', '–').replace("\\'", "'")
+    % date = '/'.join(reversed(e["date"].split('/'))) if e["date"] else ''
+    <li>{{!desc}} -- {{date}}</li>
+  % end
+  </ol>
+</div>
 </body>
